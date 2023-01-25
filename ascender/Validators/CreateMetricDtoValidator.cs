47c2d106f1f5acb7e70a08d5d904ad532b5971ba
Simen Mailund Svendsen <sima@netlight.com>
2023-01-25 17:01:29 +0100

    create validator for create metric dto

--- ascender/Validators/CreateMetricDtoValidator.cs
+++ ascender/Validators/CreateMetricDtoValidator.cs
@@ -1,6 +1,12 @@
+using ascender.Dto;
+
 namespace ascender.Validators;
 
 public class CreateMetricDtoValidator
 {
-    
+    public bool Validate(CreateMetricDto dto)
+    {
+        return dto.Window > 0;
+    }
+
 }
