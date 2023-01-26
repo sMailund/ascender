ef9f6573c8ca6e08044d4c3d2670152e5f189450
Simen Mailund Svendsen <sima@netlight.com>
2023-01-26 16:26:04 +0100

    remove redundant constructors

--- ascender/DomainObjects/Metric.cs
+++ ascender/DomainObjects/Metric.cs
@@ -8,18 +8,6 @@ public abstract class Metric
     public decimal? Minumum { get; set;  }
     public int Window { get; set;  }
 
-    public Metric()
-    {
-        
-    }
-    
-    public Metric(string name, decimal? max, int window)
-    {
-        Name = name;
-        Max = max;
-        Window = window;
-    }
-    
     public bool Validate(decimal newValue)
     {
         var withInRange = (!Max.HasValue || newValue <= Max) && (!Minumum.HasValue || newValue >= Minumum);
