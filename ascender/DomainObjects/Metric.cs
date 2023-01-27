2ccab77f66f95fd9bd3ffbb492f2236adbc70334
Simen Mailund Svendsen <sima@netlight.com>
2023-01-27 13:44:45 +0100

    clean up

--- ascender/DomainObjects/Metric.cs
+++ ascender/DomainObjects/Metric.cs
@@ -2,39 +2,31 @@ namespace ascender.DomainObjects;
 
 public abstract class Metric
 {
-    public string Name { get; set;  }
+    public string Name { get; set; }
     public decimal Cutoff { get; set; }
-    public decimal? Max { get; set;  }
-    public decimal? Minimum { get; set;  }
+    public decimal? Max { get; set; }
+    public decimal? Minimum { get; set; }
     public decimal? Threshold { get; set; }
-    public int Window { get; set;  }
+    public int Window { get; set; }
 
     public bool Validate(decimal newValue)
     {
-        var withInRange = WithinRange(newValue);
-        if (!withInRange)
+        if (!WithinRange(newValue))
         {
             return false;
         }
-        
 
         if (IsWithinThreshold(newValue))
         {
             return true;
         }
 
-        var betterThanCutoff = IsBetterThanCutoff(newValue);
-
-        return betterThanCutoff && withInRange;
+        return IsBetterThanCutoff(newValue);
     }
 
     protected abstract bool IsBetterThanCutoff(decimal newValue);
     protected abstract bool IsWithinThreshold(decimal newValue);
 
-    private bool WithinRange(decimal newValue)
-    {
-        
-        return (!Max.HasValue || newValue <= Max) && (!Minimum.HasValue || newValue >= Minimum);
-    }
-
+    private bool WithinRange(decimal newValue) =>
+        (!Max.HasValue || newValue <= Max) && (!Minimum.HasValue || newValue >= Minimum);
 }
