58d897cbe0080ebd11d258534fe55d0f764c78ad
Simen Mailund Svendsen <sima@netlight.com>
2023-01-29 18:36:42 +0100

    disallow outwards dependencies on domainentities

--- tests/architecturalTests/HexagonalArchitecture.cs
+++ tests/architecturalTests/HexagonalArchitecture.cs
@@ -16,6 +16,15 @@ public class HexagonalArchitecture
     private readonly IObjectProvider<IType> Core =
         Classes().That().ResideInNamespace("core.*", true).As("core");
 
+    private readonly IObjectProvider<IType> DomainEntities =
+        Classes().That().ResideInNamespace("core.domainEntities.*", true).As("domain entities");
+    
+    private readonly IObjectProvider<IType> Repositories =
+        Classes().That().ResideInNamespace("core.repositories.*", true).As("repositories");
+    
+    private readonly IObjectProvider<IType> Services =
+        Classes().That().ResideInNamespace("core.services.*", true).As("services");
+    
     private readonly IObjectProvider<IType> NotCore =
         Classes().That().DoNotResideInNamespace("core.*", true).As("not core");
 
@@ -24,6 +33,10 @@ public class HexagonalArchitecture
     public void ShouldUseHexagonalArchitecture()
     {
         Classes().That().Are(Core).Should().NotDependOnAny(NotCore).Check(Architecture);
+        Classes().That().Are(DomainEntities)
+            .Should().NotDependOnAny(Repositories)
+            .OrShould().NotDependOnAny(Services)
+            .Check(Architecture);
     }
     
 }
